3bfcfb091d6b2bdd8c8a1701c7272c4618904d4f
Ilkin Ibadov <ilkin.ibadov21@gmail.com>
2025-12-14 10:19:16 +0400

    project added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -29,7 +29,7 @@ const Products = () => {
 
     return (
         <div className={`${isDarkmodeActive ? "bg-slate-900 text-white" : "bg-white text-black"} transition-all duration-200`}>
-            <h1 className="m-5 text-2xl text-red-600">{t("loginTitle")}</h1>
+            <h1 className="m-5 sm:text-lg md:text-xl lg:text-2xl text-red-600">{t("loginTitle")}</h1>
             <div className="w-full flex justify-center py-5">
                 <input className={`border border-zinc-300 p-3 min-w-[300px]`} placeholder="Search for any product.." type="text" value={searchterm} onChange={(e) => {
                     setSearchterm(e.target.value)
@@ -37,7 +37,7 @@ const Products = () => {
                 <button onClick={toggleDarkmode} className="bg-red-600 text-white px-2 hover:cursor-pointer hover:bg-red-700">{isDarkmodeActive ? t('disable') : t('enable')} Darkmode</button>
                 <LanguageSelector />
             </div>
-            <div className="w-full min-h-screen h-fit grid grid-cols-4 gap-5 p-5">
+            <div className="w-full min-h-screen h-fit grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-5">
                 {products.map(product => <Card key={product._id} product={product} />)}
             </div>
         </div>
